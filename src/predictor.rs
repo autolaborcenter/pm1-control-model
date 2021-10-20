1a25b212273e843af89b7389ebf8e009f552432c
YdrMaster <ydrml@hotmail.com>
2021-10-20 11:03:32 +0800

    refactor: 删除预测器无用的方法

--- src/predictor.rs
+++ src/predictor.rs
@@ -1,21 +1,11 @@
 ﻿use crate::{optimizer::Optimizer, Physical};
 
+#[derive(Clone)]
 pub struct Predictor {
-    pub delta_rudder: f32,    // 后轮最大步进量
+    pub rudder_step: f32,     // 后轮最大步进量
     pub optimizer: Optimizer, // 优化器
     pub current: Physical,    // 当前状态
     pub target: Physical,     // 目标状态
-}
-
-impl Predictor {
-    pub fn reset(&mut self) {
-        self.current = Physical::ZERO;
-        self.target = Physical::ZERO;
-    }
-
-    pub fn set_target(&mut self, target: Physical) {
-        self.target = target;
-    }
 }
 
 impl Iterator for Predictor {
@@ -26,9 +16,9 @@ impl Iterator for Predictor {
             self.current = Physical {
                 speed: self.optimizer.optimize_speed(self.target, self.current),
                 rudder: if self.target.rudder > self.current.rudder {
-                    f32::min(self.current.rudder + self.delta_rudder, self.target.rudder)
+                    f32::min(self.current.rudder + self.rudder_step, self.target.rudder)
                 } else {
-                    f32::max(self.current.rudder - self.delta_rudder, self.target.rudder)
+                    f32::max(self.current.rudder - self.rudder_step, self.target.rudder)
                 },
             };
         }
