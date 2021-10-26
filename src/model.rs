b07a33bc68abcb6f8c593340fd3f0b1a1487c099
YdrMaster <ydrml@hotmail.com>
2021-10-26 13:22:07 +0800

    fix: 正确使用 atan2

--- src/model.rs
+++ src/model.rs
@@ -1,24 +1,21 @@
 ﻿use crate::{Physical, Velocity, Wheels};
 
 /// 描述底盘结构并用于转换控制量空间的结构体。
-/// 
+///
 /// 控制量空间一共有三种：
-/// 
 ///     三轮阿卡曼（Physical）模型：最快轮速Speed，后轮转角Rudder
 ///     质点控制（Velocity）模型：线速度V、角速度W
 ///     两轮差动控制（Wheels）模型：左轮轮速LV,右轮轮速RV
 ///
 /// 上位机控制机器人运动采用Physical模型：
-/// 
 ///     利用Physical作为控制量，转换得到左右轮速LV、RV及自身的Rudder三个控制量，发送给底盘
 ///     先用physical_to_velocity(&self, physical: Physical) -> Velocity，得到中间状态
 ///     再利用velocity_to_wheels(&self, velocity: Velocity) -> Wheels，得到LV，RV
-/// 里程计由Wheels模型转换得到Velocity模型   
-///  
+///
+/// 里程计由Wheels模型转换得到Velocity模型
 ///     利用wheels_to_velocity(&self, wheels: Wheels) -> Velocity，利用Velocity进行预测
 ///
 /// 增设临界角
-/// 
 ///     除了描述结构必要的三个参数之外，此结构体还额外缓存了临界角。
 ///     临界角的目的是控制整车最大速度，避免出现原地转时的速度远大于前进的速度。
 ///     当后轮转角（的绝对值）大于临界角，后轮相对于地面的线速度将大于前轮，
@@ -26,24 +23,25 @@
 ///     否则，`speed` 表示较快的前轮的线速度。
 #[derive(Clone)]
 pub struct ChassisModel {
-    ///左右车轮宽度，单位m
+    /// 左右车轮宽度，单位 m
     pub width: f32,
-    ///前后轮距离，单位m
+    /// 前后轮距离，单位 m
     pub length: f32,
-    ///车轮半径，单位m
+    /// 车轮半径，单位 m
     pub wheel: f32,
-    ///后轮临界角，单位rad
+    /// 后轮临界角，单位 rad
     critical_rudder: f32,
 }
-/// 加载默认底盘数据（PM1底盘）
+
 impl Default for ChassisModel {
+    /// 加载默认底盘数据（PM1底盘）
     fn default() -> Self {
         Self::new(0.465, 0.355, 0.105)
     }
 }
 
-/// 新建底盘模型
 impl ChassisModel {
+    /// 新建底盘模型
     pub fn new(width: f32, length: f32, wheel: f32) -> Self {
         Self {
             width,
@@ -55,25 +53,30 @@ impl ChassisModel {
 }
 
 impl ChassisModel {
-    ///后轮物理模型换算到线、角速度模型
+    /// 后轮物理模型换算到线、角速度模型
     pub fn physical_to_velocity(&self, physical: Physical) -> Velocity {
-        if physical.rudder == 0.0 {
+        if physical.rudder.is_nan() {
+            Velocity { v: 0.0, w: 0.0 }
+        } else if physical.rudder == 0.0 {
             Velocity {
                 v: physical.speed,
                 w: 0.0,
             }
         } else {
-            let r_chassis = -self.length / physical.rudder.tan(); //转弯半径，由轴距length及后轮转角rudder确定
+            // 底盘转弯半径
+            let r_chassis = -self.length / physical.rudder.tan();
             let w = if physical.rudder.abs() > self.critical_rudder {
-                // speed 为后轮线速度                                        //如果后轮轮速最大，角速度由后轮确定
-                let r_rudder = -self.length / physical.rudder.sin(); //后轮转弯半径r_rudder, 由轮距length及后轮转角rudder确定
-                physical.speed / r_rudder //角速度w，由线速度speed及后轮转弯半径r_rudder确定
+                // speed 为后轮线速度
+
+                // 后轮转弯半径
+                let r_rudder = -self.length / physical.rudder.sin();
+                physical.speed / r_rudder
             } else if physical.rudder > 0.0 {
                 // speed 为左轮线速度
-                physical.speed / (r_chassis - self.width / 2.0) //如果左轮轮速最大，角速度由左轮确定
+                physical.speed / (r_chassis - self.width / 2.0)
             } else {
                 // speed 为右轮线速度
-                physical.speed / (r_chassis + self.width / 2.0) //如果右轮轮速最大，角速度由右轮确定
+                physical.speed / (r_chassis + self.width / 2.0)
             };
             Velocity {
                 v: w * r_chassis,
@@ -82,29 +85,41 @@ impl ChassisModel {
         }
     }
 
-    ///线、角速度模型换算到后轮物理模型
+    /// 线、角速度模型换算到后轮物理模型
     pub fn velocity_to_physical(&self, velocity: Velocity) -> Physical {
         if velocity.w == 0.0 {
-            Physical {
-                speed: velocity.v,
-                rudder: 0.0,
+            if velocity.v == 0.0 {
+                Physical::RELEASED
+            } else {
+                Physical {
+                    speed: velocity.v,
+                    rudder: 0.0,
+                }
             }
         } else {
-            let r_chassis = velocity.v / velocity.w; //阿卡曼模型，转动中心到车辆中心的距离 r_chassis = v/w;
-            let rudder = self.length.atan2(r_chassis); //阿卡曼模型，轮转角 rudder = atan（轮距 length / 转动半径 r_chassis）
-            let speed = velocity.w                      //最大速度由当前后轮轮转角及w共同决定
+            // 底盘转弯半径
+            let r_chassis = velocity.v / velocity.w;
+            // **NOTICE**
+            // 此处需要关注 atan2 定义
+            // 为了使结果在指定的象限内，需要设计两个量的符号
+            // 此处需要 `r_chassis` 为正
+            let rudder = (r_chassis.signum() * -self.length).atan2(r_chassis.abs());
+            let speed = velocity.w
                 * if rudder.abs() > self.critical_rudder {
-                    -self.length / rudder.sin() // r_rudder
+                    // 后轮相对地面转弯半径
+                    -self.length / rudder.sin()
                 } else if rudder > 0.0 {
+                    // 右轮相对地面转弯半径
                     r_chassis - self.width / 2.0
                 } else {
+                    // 左轮相对地面转弯半径
                     r_chassis + self.width / 2.0
                 };
             Physical { speed, rudder }
         }
     }
 
-    ///左右轮速模型换算到线、角速度模型
+    /// 左右轮速模型换算到线、角速度模型
     pub fn wheels_to_velocity(&self, wheels: Wheels) -> Velocity {
         Velocity {
             v: (wheels.right + wheels.left) * self.wheel / 2.0,
@@ -112,7 +127,7 @@ impl ChassisModel {
         }
     }
 
-    ///线、角速度模型换算到左右轮速模型
+    /// 线、角速度模型换算到左右轮速模型
     pub fn velocity_to_wheels(&self, velocity: Velocity) -> Wheels {
         Wheels {
             left: (velocity.v - self.width / 2.0 * velocity.w) / self.wheel,
@@ -120,12 +135,12 @@ impl ChassisModel {
         }
     }
 
-    //后轮物理模型换算到左右轮速模型
+    /// 后轮物理模型换算到左右轮速模型
     pub fn physical_to_wheels(&self, physical: Physical) -> Wheels {
         self.velocity_to_wheels(self.physical_to_velocity(physical))
     }
 
-    ///左右轮速换算到后轮物理模型
+    /// 左右轮速换算到后轮物理模型
     pub fn wheels_to_physical(&self, wheels: Wheels) -> Physical {
         self.velocity_to_physical(self.wheels_to_velocity(wheels))
     }
@@ -151,25 +166,75 @@ mod o {
 }
 
 #[cfg(test)]
-mod tests {
-    use nalgebra::ComplexField;
+mod unit {
+    use std::f32::consts::FRAC_PI_4;
 
     use super::*;
 
+    #[inline]
+    fn float_equal(a: f32, b: f32) -> bool {
+        (a.is_nan() && b.is_nan()) || (a - b).abs() <= f32::EPSILON
+    }
+
     #[test]
     fn test_physic_to_chassis() {
-        let chassis_model = ChassisModel::new(0.4, 0.3, 0.1);
-        let phyical = Physical {
-            speed: 0.4,
-            rudder: std::f32::consts::FRAC_PI_2 - 0.1,
-        };
-        let velocity = chassis_model.physical_to_velocity(phyical);
-        let wheels = chassis_model.velocity_to_wheels(velocity);
-        let physical1 = chassis_model.wheels_to_physical(wheels);
-        let physical2 = chassis_model.velocity_to_physical(velocity);
-        assert_eq!(phyical, physical2);
+        let model: ChassisModel = ChassisModel::new(0.4, 0.3, 0.1);
+        let physicals = [
+            Physical::RELEASED,
+            // Physical::ZERO, 只要机器人不移动，统一恢复为 Physical::RELEASED
+            Physical {
+                speed: 0.5,
+                rudder: FRAC_PI_4,
+            },
+            Physical {
+                speed: 0.5,
+                rudder: -FRAC_PI_4,
+            },
+            Physical {
+                speed: -0.5,
+                rudder: FRAC_PI_4,
+            },
+            Physical {
+                speed: -0.5,
+                rudder: -FRAC_PI_4,
+            },
+        ];
 
-        assert!((std::f32::consts::PI - physical2.rudder - phyical.rudder).abs() < 1e-3);
-        assert!((std::f32::consts::PI - physical1.rudder - phyical.rudder).abs() < 1e-3);
+        for physical in physicals {
+            {
+                // 验证 Physical 与 Velocity 互转
+                let velocity = model.physical_to_velocity(physical);
+                let restore = model.velocity_to_physical(velocity);
+                assert!(
+                    float_equal(physical.speed, restore.speed),
+                    "{:?} != {:?}",
+                    physical.speed,
+                    restore.speed
+                );
+                assert!(
+                    float_equal(physical.rudder, restore.rudder),
+                    "{:?} != {:?}",
+                    physical.rudder,
+                    restore.rudder
+                );
+            }
+            {
+                // 验证 Physical 与 Wheels 互转
+                let wheels = model.physical_to_wheels(physical);
+                let restore = model.wheels_to_physical(wheels);
+                assert!(
+                    float_equal(physical.speed, restore.speed),
+                    "{:?} != {:?}",
+                    physical.speed,
+                    restore.speed
+                );
+                assert!(
+                    float_equal(physical.rudder, restore.rudder),
+                    "{:?} != {:?}",
+                    physical.rudder,
+                    restore.rudder
+                );
+            }
+        }
     }
 }
